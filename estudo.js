efeedc62ab223704b893b8a6934c235690d28a41
gg <108942341+GoliasLegal@users.noreply.github.com>
2022-10-15 17:50:09 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,13 +1,13 @@
-function total(){
-   let c = document.getElementsByClassName("valor").value;
-   let j = document.getElementsByClassName("juros").value;
-   let t = document.getElementsByClassName("meses").value
-   let r = 0;
-   for(let i = 1; i <= t; i++){
-      r = c * (1 + (j/100));
-      document.write("mes: "+ i + "valor:" + r +)"<brs";
-      v = r;
-   }
+ function total() {
+     let c = document.getElementById("valor").value;
+     let j = document.getElementById("juros").value;
+     let t = document.getElementById("meses").value;
+     let r = 0;
+     for (let i = 1; i <= t; i++) {
+           r = c * (1 + (j/100));
+       document.write("Mês " + i + " valor: " + r + "<br>");
+           c = r;
+     }
 
-   document.write("Resultado: " + r);
+     document.write("Resultado: " + r);
 }
