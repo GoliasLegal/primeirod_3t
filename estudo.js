a7ac3a815e1b39d2a7ff31498b04cda0ce87165c
gg <108942341+GoliasLegal@users.noreply.github.com>
2022-10-15 17:54:43 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -1,13 +1,12 @@
- function total() {
-     let c = document.getElementById("valor").value;
-     let j = document.getElementById("juros").value;
-     let t = document.getElementById("meses").value;
-     let r = 0;
-     for (let i = 1; i <= t; i++) {
-           r = c * (1 + (j/100));
-       document.write("Mês " + i + " valor: " + r + "<br>");
-           c = r;
-     }
-
-     document.write("Resultado: " + r);
+function total(){
+    let c = document.getElementById("capital").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("meses").value;
+    let m = 0;
+    for(let i = 1; i <= t; i++){
+        m = c * (1 + (j/100));
+        document.write("Valor no mês " + i + " = " + m + "<br>");
+        c = m;
+    }
+    document.write("Montante: " + m);
 }
